627a9686e25982ac8bae0f247e0f715f4f62b88c
Eddie Rosas <eddie@hfa3.org>
2022-06-27 18:53:52 -0700

    tdd for top_3 filter

--- lib/process_matches.rb
+++ lib/process_matches.rb
@@ -11,6 +11,15 @@ def parse_match(match)
   [[team1_name, team1_score], [team2_name, team2_score]]
 end
 
+def top_3_for_match_day(rankings)
+  rankings.sort_by { |t, p| [-p, t] }
+          .first(3)
+          .map do |team, league_points|
+            points = league_points == 1 ? "pt" : "pts"
+            "#{team}, #{league_points} #{points}"
+          end.join("\n")
+end
+
 def print_top_teams(matches)
 
 end
--- test/test_process_matches.rb
+++ test/test_process_matches.rb
@@ -23,3 +23,33 @@ describe "parse_match" do
     end
   end
 end
+
+describe "top_3_for_match_day" do
+  describe "given a cumulative set of matchday rankings" do
+    it "returns the top 3 teams in alphabetical order" do
+      rankings = {
+        "San Jose Earthquakes" => 4,
+        "Aptos FC" => 4,
+        "Capitola Seahorses" => 0,
+        "Santa Cruz Slugs" => 6,
+        "Monterey United" => 2,
+        "Felton Lumberjacks" => 1
+      }
+      expected = "Santa Cruz Slugs, 6 pts\nAptos FC, 4 pts\nSan Jose Earthquakes, 4 pts"
+      assert_equal(expected, top_3_for_match_day(rankings))
+    end
+    
+    it "handles pluralization of points" do
+      rankings = {
+        "San Jose Earthquakes" => 1,
+        "Aptos FC" => 1,
+        "Capitola Seahorses" => 1,
+        "Santa Cruz Slugs" => 6,
+        "Monterey United" => 1,
+        "Felton Lumberjacks" => 1
+      }
+      expected = "Santa Cruz Slugs, 6 pts\nAptos FC, 1 pt\nCapitola Seahorses, 1 pt"
+      assert_equal(expected, top_3_for_match_day(rankings))
+    end
+  end
+end
