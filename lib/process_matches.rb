d313cff720983e7d9e940dadeb6dfda7bf4b1ec2
Eddie Rosas <eddie@hfa3.org>
2022-06-27 19:41:28 -0700

    complete method to pass main feature test

--- lib/process_matches.rb
+++ lib/process_matches.rb
@@ -20,6 +20,53 @@ def top_3_for_match_day(rankings)
           end.join("\n")
 end
 
-def print_top_teams(matches)
+def print_top_teams(match_results)
+  teams = Hash.new
+  rankings = Hash.new(0)
+  matchday = 0
 
+  File.open(match_results).each do |match|
+    parsed_match = parse_match(match)
+
+    team1, team2 = parsed_match
+    team1_name, team1_score = team1
+    team2_name, team2_score = team2
+
+    # A matchday has ended when we begin to see teams from our team hash
+    if teams[team1_name] || teams[team2_name]
+      teams.each do |team, league_points|
+        rankings[team] += league_points
+      end
+
+      matchday += 1
+      puts "Matchday #{matchday}"
+      puts top_3_for_match_day(rankings)
+      puts ("\n")
+
+      # reset the team hash for the new matchday
+      teams = Hash.new
+    end
+
+    # League point accumulation
+    if team1_score > team2_score
+      teams[team1_name] = 3
+      teams[team2_name] = 0
+    elsif team2_score > team1_score
+      teams[team2_name] = 3
+      teams[team1_name] = 0
+    else
+      teams[team1_name] = 1
+      teams[team2_name] = 1
+    end
+  end
+
+  # Last matchday processing
+  teams.each do |team, league_points|
+    rankings[team] += league_points
+  end
+
+  matchday += 1
+  puts "Matchday #{matchday}"
+  puts top_3_for_match_day(rankings)
+  puts ("\n")
 end
