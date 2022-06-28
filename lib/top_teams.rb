c44a64acacdcbf1487694909c4e900ad1258c510
Eddie Rosas <eddie@hfa3.org>
2022-06-27 20:12:02 -0700

    add file extension check to execution method

--- lib/top_teams.rb
+++ lib/top_teams.rb
@@ -2,6 +2,9 @@ require_relative "process_matches"
 
 def top_teams
   matches_file = ARGV[0]
+  extension = File.extname(matches_file)
+  raise ("Input must be a text file") if extension != ".txt"
+
   print_top_teams(matches_file)
 end
 
