1db7b106452066634ebc464b840261e92472d534
Eddie Rosas <eddie@hfa3.org>
2022-06-27 18:12:31 -0700

    tdd for basic parsing function

--- lib/process_matches.rb
+++ lib/process_matches.rb
@@ -1,3 +1,16 @@
+def parse_match(match)
+  team1, team2 = match.split(",")
+
+  score_pattern = /\s+\d+$/
+  team1_name = team1.sub(score_pattern, "").strip
+  team1_score = team1.scan(score_pattern)[0].strip.to_i
+
+  team2_name = team2.sub(score_pattern, "").strip
+  team2_score = team2.scan(score_pattern)[0].strip.to_i
+  
+  [[team1_name, team1_score], [team2_name, team2_score]]
+end
+
 def print_top_teams(matches)
 
 end
--- test/test_process_matches.rb
+++ test/test_process_matches.rb
@@ -11,5 +11,15 @@ describe "print_top_teams" do
     it "prints cumulative matchday score results" do
       assert_output(@expected_output) { print_top_teams(@input) }
     end
-  end 
+  end
+end
+
+describe "parse_match" do
+  describe "given a valid match result" do
+    valid_match = "San Jose Earthquakes 3, Santa Cruz Slugs 3"
+    expected_sets = [["San Jose Earthquakes", 3], ["Santa Cruz Slugs", 3]]
+    it "returns a pair of teams and their respective scores" do
+      assert_equal(expected_sets, parse_match(valid_match))
+    end
+  end
 end
